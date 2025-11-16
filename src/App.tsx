e1b555d0e4df4a9f7cef20201584e970c97e4bf1
Maria Helena Welk <156597965+sansonpedro@users.noreply.github.com>
2025-11-15 22:46:08 -0300

    corrigindo export

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
 export function App() {
-  console.log('Oi');
-
   return (
-    <>
+    <div>
       <h1>Ola mundo!</h1>\
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum
-        deserunt maxime non possimus tenetur officiis velit rerum consequatur
-        numquam dicta, minus accusamus ea debitis sunt? Suscipit officia id
-        quasi vel?
-      </p>
-    </>
+    </div>
   );
 }
-
-// commit
